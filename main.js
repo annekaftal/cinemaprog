b47bed2de1f623892e40597afbd0bfc9d5c3e040
Anne Kaftal <annekaftal@Air-de-Anne.lan>
2024-07-29 18:43:05 +0200

    première version barre de recherche

--- main.js
+++ main.js
@@ -4,13 +4,23 @@ const options = {
       accept: 'application/json',
       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMWU4MjY5MzkxNWM0MjkyYWQzYzZkYmQwYTIwOGYwNCIsIm5iZiI6MTcyMjI0ODMzNS4yMTA2MjQsInN1YiI6IjY2YTc2NWRiNzZlNTJjODY0MDNhNDAyYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ryYOaPsrZLJmv_u060JOPOHHQjnOxaU-JK4AibGhlbA'
     }
-  };
-
-const getData = async () => {
-    const response = await fetch('https://api.themoviedb.org/3/search/movie?query=almost+famous&include_adult=false&language=en-US&page=1', options);
-    const data = await response.json();
-    console.log(data.results[0]);
-    document.querySelector("#displayMovie").innerHTML = `${data.results[0].original_title}, ${data.results[0].release_date}.`;
 };
 
-getData();
+
+let input = document.querySelector('input')
+let matches = document.querySelector('#movieMatches');
+input.addEventListener('input', async () => {
+    console.log(`input = ${input.value}`);
+    const response = await fetch(`https://api.themoviedb.org/3/search/movie?query=${input.value}&include_adult=false&language=en-US&page=1`, options);
+    const data = await response.json();
+    if (matches.innerHTML != null) {
+        matches.innerHTML = null;
+    };
+    for (let i = 0; i < data.results.length; i++){
+        let movieMatch = document.createElement('div');
+        movieMatch.classList.add('match');
+        movieMatch.setAttribute('id', `movie${data.results[i].id}`)
+        movieMatch.innerHTML = `${data.results[i].original_title}, ${data.results[i].release_date}<br/>`;
+        matches.appendChild(movieMatch); 
+    };
+});
